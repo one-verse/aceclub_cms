ba3f3347c3e7cbb701bd7a7d9c852a67cdb06489
goutham.pogaku@spartanpoker.com <Vivan@0212>
2025-04-11 15:51:17 +0530

    modified background color and font color to pages

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -22,12 +22,12 @@ export default async function Page({ params }: { params: { slug: string } }) {
     );
   }
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen ">
     <div className="container mx-auto max-w-[1200px] px-4">
-      <p className="h1 font-sans text-light my-5 text-center text-white text-5xl">
+      <p className="h1 font-sans text-light my-5 text-center text-5xl text-white">
         {pageData.title}
       </p>
-      <div className="my-3" >
+      <div className="my-3" style={{ color: "#CCCCCC" }}>
         {pageData.DynamicZone && (
           <DynamicZoneManager dynamicZone={pageData.DynamicZone} />
         )}
