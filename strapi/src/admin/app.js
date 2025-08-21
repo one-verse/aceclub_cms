5ac0a91bce42bf229eead3c1dd6bb7e79f688af1
Yvijay Kumar <vijay.kumar@spartanpoker.com>
2025-08-21 12:12:31 +0530

    card sniper changes done

--- strapi/src/admin/app.js
+++ strapi/src/admin/app.js
@@ -14,8 +14,8 @@ const config = {
   },
   translations: {
     en: {
-      "app.components.LeftMenu.navbrand.title": "ACE CLUB POKER",
-      "Auth.form.welcome.subtitle": "ACE CLUB POKER",
+      "app.components.LeftMenu.navbrand.title": "Sniper Poker",
+      "Auth.form.welcome.subtitle": "Sniper Poker",
       "Auth.form.welcome.title": "Admin Access",
     },
   },
