c4f24fcd1def132d12c3c1879cf513a63d6feabd
Harish Tadka - 1verse <116143141+harish-spartanpoker@users.noreply.github.com>
2025-11-11 16:49:14 +0530

    chore : added tawk chat widget

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -49,7 +49,17 @@ export default async function RootLayout({
         <main className="">{children}</main>
         <Footer FooterData={pageData.footer} />
         {/* <Script src="https://launchers.bonb.io/support.js?id=BQPIA5MFLr" defer></Script> */}
-        <Script src="https://launchers.bonb.io/support.js?id=-IRvIAxNzt" defer></Script>
+        <Script id="tawkto-chat" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html:`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/6912e919f6ff90195b6c914f/1j9ott3kh';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();`}}
+          />
       </body>
     </html>
   );
